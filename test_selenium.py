6ff5ad1277d937b861c67f55936bcdc01c3bbdc8
Łukasz Saletnik <luksaletnik@gmail.com>
2023-09-27 19:53:49 +0200

    Added 'chromedriver.exe'

--- test_selenium.py
+++ test_selenium.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# from webdriver_manager.chrome import ChromeDriverManager
+# Zastosowanie Webdriver Manager
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver = webdriver.Chrome()
