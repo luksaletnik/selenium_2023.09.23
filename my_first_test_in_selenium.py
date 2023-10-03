96a5f825067f5780356dd1a70fd63e35676d3859
Łukasz Saletnik <luksaletnik@gmail.com>
2023-10-03 19:22:14 +0200

    Updated file

--- my_first_test_in_selenium.py
+++ my_first_test_in_selenium.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys
 
 # Zastosowanie Webdriver Manager:
 # from webdriver_manager.chrome import ChromeDriverManager
@@ -11,11 +12,17 @@ driver = webdriver.Chrome()
 driver.get('https://www.google.com')
 
 driver.maximize_window()
+driver.set_window_size(1024, 768)
 
 accept = driver.find_element('xpath', '//*[@id="L2AGLb"]/div')
 accept.click()
 
-time.sleep(60)
+search_box = driver.find_element('name', 'q')
+
+search_box.send_keys('Pinta Hop Selection')
+search_box.send_keys(Keys.ENTER)
+
+time.sleep(600)
 
 # Zamkniecie wszystkich okien przegladarki
 # driver.quit()
